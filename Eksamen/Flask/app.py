3efea122608126751716f4cc16b74e7c453ba38e
unknown <hajjari057@gmail.com>
2026-05-05 12:35:40 +0200

    Fiksing av flask

--- Eksamen/Flask/app.py
+++ Eksamen/Flask/app.py
@@ -7,17 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hjem():
-    with mariadb.connect(
-            user=os.getenv("DB_USER"),
+    try:
+        with mariadb.connect(
+        user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             host="localhost",
             port=3306,
-            database=os.getenv("DB_NAME") ) as conn:
+            database=os.getenv("DB_NAME")) as conn:
 
             mycursor = conn.cursor()
             mycursor.execute("SELECT * FROM produkter")
             produkter = mycursor.fetchall()
 
+            return render_template('index.html', produkter=produkter)
+
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB: {e}")
+        return "Kunne ikke koble til databasen", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
